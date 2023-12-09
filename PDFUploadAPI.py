23b4c9b22965d3bc72428bf821d509b9eb5c20b6
Sanjeev <ramansanjeevi97@gmail.com>
2023-12-10 01:02:55 +0530

    DB Connectivity

--- PDFUploadAPI.py
+++ PDFUploadAPI.py
@@ -5,17 +5,33 @@ from langchain.embeddings import HuggingFaceInstructEmbeddings
 from dotenv import load_dotenv
 #from langchain.vectorstores import FAISS
 import os
+import pymongo
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = {'pdf'}
 
+client = pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1")
+
+db = client.get_database("ChatBot")
+
+collection = db["ChunkData"]
+
 def get_vectorstore(text_chunks):
     embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     #embeddings = OpenAIEmbeddings() //chargeable ==> /1000 token = 0.004$
     #vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return embeddings
+
+def upload_chunk_data(pdf_name,text_chunk):
+  upload_data = {
+    "PDFFileName": pdf_name,
+    "JunkData": text_chunk
+  }
+  result = collection.insert_one(upload_data)
+  print("Inserted document with ID: {result.inserted_id}")
+  
 
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
@@ -30,14 +46,11 @@ def get_text_chunks(text):
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def get_pdf_text(pdf_docs):
+def get_pdf_raw_text(pdf_doc):
     text = ""
-    for pdf in pdf_docs:
-        if pdf and allowed_file(pdf.filename):
-            pdf_reader = PdfReader(pdf)
-            for page in pdf_reader.pages:
-                text += page.extract_text()
-            
+    pdf_reader = PdfReader(pdf_doc)
+    for page in pdf_reader.pages:
+      text += page.extract_text()
     return text
 
 @app.route('/upload/PDF_Files', methods=['POST'])
@@ -48,17 +61,16 @@ def upload_files():
 
     pdf_docs = request.files.getlist('files')
 
-    # get pdf text
-    raw_text = get_pdf_text(pdf_docs)
+    for pdf_doc in pdf_docs:
+      if pdf_doc and allowed_file(pdf_doc.filename):
+        pdf_fileName = os.path.splitext(pdf_doc.filename)[0]
+        raw_text = get_pdf_raw_text(pdf_doc)
+        text_chunks = get_text_chunks(raw_text)
+        upload_chunk_data(pdf_fileName,text_chunks)
+        #vectorstore = get_vectorstore(text_chunks)
 
-    # get the text chunks
-    text_chunks = get_text_chunks(raw_text)
-
-    # create vector store - issue in free source
-    #vectorstore = get_vectorstore(text_chunks)
-
-
-    return jsonify(text_chunks), 200
+    client.close()
+    return jsonify("All PDF files are uploaded successfully"), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
